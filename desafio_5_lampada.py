31b52ac00e47aca9984ffa2230961a3c2e57a456
Yago Castello <104936515+YagoCastello@users.noreply.github.com>
2024-10-06 18:06:05 -0300

    Create desafio_5_lampada.py

---
+++ desafio_5_lampada.py
@@ -0,0 +1,12 @@
+print("""
+      Primeiro passo: Ligue o primeiro interruptor e deixe ligado por um tempo, cerca de 5 minutos, para que a lâmpada correspondente aqueça.
+
+Segundo passo: Após o tempo passar, desligue o primeiro interruptor e ligue o segundo interruptor.
+
+Terceiro passo: Vá para a sala das lâmpadas.
+
+A lâmpada que está acesa será controlada pelo segundo interruptor (o que ficou ligado antes de você ir).
+A lâmpada que está apagada, mas quente, será controlada pelo primeiro interruptor (o que foi ligado por um tempo e depois desligado).
+A lâmpada que está apagada e fria será controlada pelo terceiro interruptor (o que não foi ligado em nenhum momento).
+      
+      """)
